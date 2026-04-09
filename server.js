4afa45f9068ef8f322036abc3bc23d63ed2d8177
ansarisuhel4505 <ansarisuhel4505@gmail.com>
2026-04-09 15:32:28 +0530

    Update server.js

--- server.js
+++ server.js
@@ -64,6 +64,9 @@ app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
-app.listen(PORT, () => {
-  console.log(`🚀 Server running on http://localhost:${PORT}`);
-});
+module.exports = app;
+if (process.env.NODE_ENV !== 'production') {
+    app.listen(PORT, () => {
+        console.log(`🚀 Server running on http://localhost:${PORT}`);
+    });
+}
